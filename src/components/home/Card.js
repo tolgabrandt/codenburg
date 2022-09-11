aaa61417a56e33e3cefa317d09a419916042c498
Tolga Brandt <tolgabrandt@gmail.com>
2022-09-11 05:03:05 +0300

    added redirects

--- src/components/home/Card.js
+++ src/components/home/Card.js
@@ -2,7 +2,11 @@ const Card = ({ CardIMG }) => {
   return (
     <li className="max-w-[380px] flex flex-col gap-8 ">
       <div className="h-[220px] bg-slate-100">
-        <img className="h-full object-cover" src={CardIMG} />
+        <img
+          className="h-full object-cover"
+          src={CardIMG}
+          alt="Blog Cover"
+        />
       </div>
       <div className="flex flex-col gap-4 px-1">
         <h2 className="text-2xl font-bold">Make React Apps More Dynamic</h2>
